9ff57708ed874f7465051abc8824aa18df05bf86
mohammad adeeb <mdadeeb7866@gmail.com>
2025-10-02 16:39:31 +0545

    Formmated files

--- Utils/ExpressErrors.js
+++ Utils/ExpressErrors.js
@@ -1,9 +1,9 @@
-class ExpressError extends Error{
-    constructor(ststusCode, message) {
-        super();
-        this.statusCode = ststusCode;
-        this.message = message;
-    }
+class ExpressError extends Error {
+  constructor(ststusCode, message) {
+    super();
+    this.statusCode = ststusCode;
+    this.message = message;
+  }
 }
 
 module.exports = ExpressError;
--- routes/review.js
+++ routes/review.js
@@ -1,17 +1,31 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const ExpressError = require("../Utils/ExpressErrors.js");
 const wrapAsync = require("../Utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
-const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
 
 const reviewController = require("../controller/reviews.js");
 
 //Reviews - Post Route
-router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
 
 //Reviews - Delete Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview));
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
 
 module.exports = router;
